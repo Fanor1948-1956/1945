66826fc91c0490e9bee724bf663837706e6cf631
Fanorlenin adadasdasd <fanorlenin31@gmail.com>
2024-10-15 22:25:35 +0000

    sd

--- config/database.js
+++ config/database.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://fanoro1945:fanoro1945@cluster0.j7bgf.mongodb.net/andres?retryWrites=true&w=majority'
-    );
+    await mongoose.connect("mongodb://localhost:27017/clinic");
     console.log('Conectado a MongoDB');
   } catch (error) {
     console.error('Error conectando a MongoDB', error);

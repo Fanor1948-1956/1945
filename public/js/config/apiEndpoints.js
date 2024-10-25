696aa26ee3c25f3130a56d8dd76ba17ce42337d0
Fanoro1956 Choque Aguirre <fanoro1956@gmail.com>
2024-10-25 05:26:01 +0000

    sad

--- public/js/config/apiEndpoints.js
+++ public/js/config/apiEndpoints.js
@@ -1,28 +1,26 @@
-
 export const BASE_URL =
   'https://7000-fanor19481956-1945-0hhln0kcvpo.ws-us116.gitpod.io';
 
 export const permissionEndpoints = {
   list: `${BASE_URL}/permissions/api`,
   create: `${BASE_URL}/permissions/create-permission`,
-  update: `${BASE_URL}/permissions/update-permission`, 
-  delete: `${BASE_URL}/permissions/delete`, 
+  update: `${BASE_URL}/permissions/update-permission`,
+  delete: `${BASE_URL}/permissions/delete`,
 };
 
 export const roleEndpoints = {
   list: `${BASE_URL}/roles/api`,
   create: `${BASE_URL}/roles/create-role`,
-  update: `${BASE_URL}/roles/update-role`, 
-  delete: `${BASE_URL}/roles/delete`, 
+  update: `${BASE_URL}/roles/update-role`,
+  delete: `${BASE_URL}/roles/delete`,
 };
 
 export const userEndpoints = {
   list: `${BASE_URL}/users/getUsers`,
   create: `${BASE_URL}/users/create`,
-  update: `${BASE_URL}/users/update`, 
-  delete: `${BASE_URL}/users/delete`, 
+  update: `${BASE_URL}/users/update`,
+  delete: `${BASE_URL}/users/delete`,
 };
-
 
 export const profileEndpoints = {
   get: `${BASE_URL}/api/profile`,
@@ -32,7 +30,6 @@ export const profileEndpoints = {
 export const specialtyEndpoints = {
   list: `${BASE_URL}/specialties/api`,
   create: `${BASE_URL}/specialties/create-specialty`,
-  update: `${BASE_URL}/specialties/update-specialty`, 
-  delete: `${BASE_URL}/specialties/delete`, 
+  update: `${BASE_URL}/specialties/update-specialty`,
+  delete: `${BASE_URL}/specialties/delete`,
 };
-

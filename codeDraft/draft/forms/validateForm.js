bd3794f3db20fd79e94476d711faf402fbafd889
Fanor <aguirrefanor31@gmail.com>
2024-11-03 00:01:01 -0400

    sd

--- codeDraft/draft/forms/validateForm.js
+++
@@ -1,9 +0,0 @@
-
-export function validateFormData(formData, requiredFields) {
-  for (let field of requiredFields) {
-    if (!formData[field]) {
-      return { valid: false, message: `El campo ${field} es obligatorio.` };
-    }
-  }
-  return { valid: true, message: '' };
-}
